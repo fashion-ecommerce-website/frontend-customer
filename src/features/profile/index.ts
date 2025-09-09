e5123b38bfa27e04a53a8319a45679aea8616709
phuclh <phuclh121003@gmail.com>
2025-09-09 16:46:32 +0700

    remove no need comment

--- src/features/profile/index.ts
+++ src/features/profile/index.ts
@@ -1,26 +1,16 @@
-// Profile Feature Barrel Export
-// Self-contained profile module exports
-
-// Export containers
 export { ProfileContainer } from './containers/ProfileContainer';
 
-// Export components
 export { ProfilePresenter } from './components/ProfilePresenter';
 export { ProfileSidebar } from './components/ProfileSidebar';
 export { ProfileFormSection } from './components/ProfileFormSection';
 export { PasswordChangeModal } from './components/PasswordChangeModal';
 export { UpdateInfoModal } from './components/UpdateInfoModal';
 
-// Export hooks
 export { useFormValidation, useProfileValidation, usePasswordValidation } from './hooks/useValidation';
 
-// Export states
-// ProfileCallState removed - no longer needed
 
-// Export redux
 export { 
   profileReducer,
-  // getProfileRequest removed - no longer needed
   getProfileSuccess,
   getProfileFailure,
   updateProfileRequest,
